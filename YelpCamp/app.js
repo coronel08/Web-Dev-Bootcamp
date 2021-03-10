dc0cd85f9ca8b972f810b027f6238c51fbcf4aa9
Fidel C <coronel08@aol.com>
2021-03-10 12:39:01 -0800

    Breakup app into Routes for reviews/campgrounds

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,44 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
-const Review = require('./models/review')
 const methodOverride = require('method-override')
+// Mongoose delete settings, used in review delete path/route
+mongoose.set('useFindAndModify', false);
 
+// Import routes/paths 
+const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
+
+// Ejsmate imported to handle ejs partials in template
 const ejsMate = require('ejs-mate')
 const { nextTick } = require('process')
 
 // error handling 
 const wrapAsync = require('./utils/wrapAsync')
 const ExpressError = require('./utils/ExpressError')
-const { campgroundSchema, reviewSchema } = require('./schema')
 const campground = require('./models/campground')
 module.exports.campgroundSchema
-
-// Joi library, error checking for adding campgrounds, validation on server side
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
-// Joi library, error checking for reviews
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
-// Mongoose delete settings, used in review delete path/route
-mongoose.set('useFindAndModify', false);
 
 // Middleware for views engine
 app.engine('ejs', ejsMate)
@@ -56,76 +35,17 @@ mongoose.connect('mongodb://root:example@localhost:27017/yelp-camp?authSource=ad
         console.log("Connection to MongoDB worked")
     }).catch(err => {
         console.log('We have an error ', err)
-    })
+})
+
+// Route for campgrounds, prepend /campgrounds, imported from routes/campgrounds
+app.use('/campgrounds', campgrounds)
+// Route for reviews
+app.use('/campgrounds/:id/reviews', reviews)
 
 // Just making a response for landing page
 app.get('/', (req, res) => {
     res.render('home')
 })
-
-// Find all campgrounds and return a template that iterates over object
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', { campgrounds })
-})
-
-// Add new campground route from form in campgrounds/new.ejs
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new')
-})
-
-// Added app.use express url encoded to be able to parse req.body
-app.post('/campgrounds', validateCampground, wrapAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground.id}`)
-}))
-
-// Display a camgrounds detail by using id
-app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
-    res.render('campgrounds/show', { campground })
-}))
-
-// Edit path/route form
-app.get('/campgrounds/:id/edit', wrapAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render(`campgrounds/edit`, { campground })
-}))
-
-// Path to accept edit information
-app.put('/campgrounds/:id', validateCampground, wrapAsync(async (req, res) => {
-    const { id } = req.params
-    // take spread of object from req.body.campground in edit.ejs form
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-// Delete path/route
-app.delete('/campgrounds/:id', wrapAsync(async (req, res) => {
-    const { id } = req.params
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
-
-// Path/route for posting review from show.ejs
-app.post('/campgrounds/:id/reviews', validateReview, wrapAsync(async(req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-)
-
-// delete path/route for reviews
-app.delete('/campgrounds/:id/reviews/:reviewId', wrapAsync(async(req,res) => {
-    const {id, reviewId} = req.params
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
 
 // Middleware for 404 
 app.all('*', (req, res, next) => {
---
+++ YelpCamp/routes/reviews.js
@@ -0,0 +1,42 @@
+const express = require('express')
+// need to use mergeParams to be able to get review param/id when adding a new review
+const router = express.Router({mergeParams:true})
+// Import models and schema
+const Campground = require('../models/campground')
+const Review = require('../models/review')
+const { reviewSchema } = require('../schema')
+// Error handling wrapper
+const wrapAsync = require('../utils/wrapAsync')
+
+
+// Joi library, error checking for reviews
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+// Path/route for posting review from show.ejs
+router.post('/', validateReview, wrapAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+)
+
+// delete path/route for reviews
+router.delete('/:reviewId', wrapAsync(async(req,res) => {
+    const {id, reviewId} = req.params
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+module.exports = router
