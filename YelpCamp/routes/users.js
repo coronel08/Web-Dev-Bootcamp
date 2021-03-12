c30020c4cc2d9f3a2980b5ab5e1d2f889778378b
Coronel08 <coronel08@aol.com>
2021-03-11 23:32:16 -0800

    made register login user, + redirect to prev page

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -13,8 +13,12 @@ router.post('/register', wrapAsync(async (req, res) => {
         const { username, email, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to Yelp Camp!')
-        res.redirect('/campgrounds')
+        // req.login, logs user in after being registered
+        req.login(registeredUser, err => {
+            if(err) return next(err)
+            req.flash('success', 'Welcome to Yelp Camp!')
+            res.redirect('/campgrounds')
+        })
     } catch (err) {
         req.flash('error', err.message)
         res.redirect('/register')
@@ -27,7 +31,10 @@ router.get('/login', async (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back')
-    res.redirect('/campgrounds')
+    const redirectUrl = req.session.returnTo || '/campgrounds'
+    // clear session in case of refresh
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req,res) => {
