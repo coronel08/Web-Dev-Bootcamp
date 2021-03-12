140fbc37d1db6243d4aa8db63a89291ba67c3bd9
Coronel08 <coronel08@aol.com>
2021-03-11 19:10:20 -0800

    register route hashes/ salts/ and adds to db

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const { remove } = require('../models/user')
+const wrapAsync = require('../utils/wrapAsync')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', (req,res) => {
-    res.send(req.body)
-})
+router.post('/register', wrapAsync(async (req, res) => {
+    try{
+        const { username, email, password } = req.body
+        const user = new User({ email, username })
+        const registeredUser = await User.register(user, password)
+        req.flash('success', 'Welcome to Yelp Camp!')
+        res.redirect('/campgrounds')
+    } catch(err){
+        req.flash('error', err.message)
+        res.redirect('/register')
+    }
+}))
 
 module.exports = router
