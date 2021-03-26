1ca33b771f6073c65f31e03ccc0f11cab184bf88
Fidel C <coronel08@aol.com>
2021-03-26 00:58:06 -0700

    Starting refractoring in seeds, then models.

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
+const venues = require('./venues')
+
 
 // Mongoose connection and catch/error 
 mongoose.connect('mongodb://root:example@localhost:27017/yelp-camp?authSource=admin',
-    {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true})
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
-    console.log('Database seeded information succesfully')
+    console.log('Database connected succesfully')
 })
 
-// function used to create random titles for camp, used below
-const sample = array => array[Math.floor(Math.random() * array.length)]
 
-// Delete data in Campground, and make new random data
-const seedDB = async () => {
-    await Campground.deleteMany({})
-    for (let i=0; i < 50; i++){
+const makeRandomEntries = async () => {
+    // function used to create random titles for camp, used below
+    const sample = array => array[Math.floor(Math.random() * array.length)]
+
+    for (let i = 0; i < 5; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
                 {
-                    url:'https://source.unsplash.com/collection/483251/1600x900',
-                    filename:'Yelpcamp/syhrnws2zsep1cagawm2'
+                    url: 'https://source.unsplash.com/collection/4989517/1600x900',
+                    filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
                 },
                 {
-                    url:'https://source.unsplash.com/1600x900/?nature,water',
-                    filename:'Yelpcamp/f1wcxybqf3ramyegzlwf'
+                    url:'https://source.unsplash.com/collection/10529343/punk',
+                    filename: 'Yelpcamp/'
                 }
             ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aperiam voluptates asperiores! Expedita fugit laudantium voluptatem vero atque animi ab earum cum eaque facilis ipsam eius rerum, distinctio magnam consequuntur corporis aperiam laboriosam dignissimos laborum. Excepturi maiores pariatur neque et.',
@@ -38,17 +39,47 @@ const seedDB = async () => {
             geometry: {
                 type: "Point",
                 coordinates: [
-                    cities[random1000].longitude, 
+                    cities[random1000].longitude,
                     cities[random1000].latitude
                 ]
-            } ,
+            },
             author: '604fda1ddd91d22464f03c9f'
         })
         await camp.save()
     }
 }
 
+const makeEntries = async () => {
+    for (let venue of venues) {
+        const camp = new Campground({
+            location: venue.location,
+            title: venue.title,
+            images: venue.images,
+            description: venue.description,
+            price: venue.price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    venue.geometry.coordinates[0],
+                    venue.geometry.coordinates[1]
+                ]
+            },
+            author: venue.author
+        })
+        await camp.save()
+    }
+}
+
+// Delete data in Campground, and make new random data
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    await makeEntries()
+    await makeRandomEntries()
+}
+
+
 //  Close database connection, returns a promise since we made it async
 seedDB().then(() => {
     mongoose.connection.close()
+    console.log('Database was seeded correctly')
 })
---
+++ YelpCamp/seeds/venues.js
@@ -0,0 +1,143 @@
+module.exports = [
+    {
+        location: `Downtown Los Angeles, California`,
+        title: `The Smell`,
+        images: [
+            {
+                url: 'https://64.media.tumblr.com/feb8247a685ca5e061a792af672cb543/tumblr_o1bvcwkHoa1qctz57o10_1280.jpg',
+                filename: 'Yelpcamp/syhrnws2zsep1cagawm2'
+            },
+            {
+                url: 'https://i1.wp.com/www.laweekly.com/wp-content/uploads/2019/06/2831449_3000770.0.jpg?fit=565%2C375&ssl=1',
+                filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
+            },
+            {
+                url: 'https://i2.wp.com/www.lataco.com/wp-content/uploads/0_4200_0_2800_one_los-angeles-the-smell-art-space-ghioldi092.jpg?resize=995%2C600&',
+                filename: 'Yelpcamp/'
+            }
+        ],
+        description: "What can I say? The bathroom says it all, a smell of concentrated urine escapes the venue all the way to the alley which coincidentally also smells like concentrated urine. It's one of the most popular venues around Los Angeles, mainly due to it being all ages and cheap. My favorite show here was probably the FYF Fest pre-show in 2011 with Title Fight and Touche Amore. Conveniently located next to Nuevo Jalisco, a dive bar that puts on drag shows during the weekends.",
+        price: 5,
+        geometry: {
+            type: "Point",
+            coordinates: [
+                -118.2436849,
+                34.0522342
+            ]
+        },
+        author: '604fda1ddd91d22464f03c9f'
+    },
+    {
+        location: `Berkeley, California`,
+        title: `924 Gilman`,
+        images: [
+            {
+                url: 'https://cdn.sfstation.com/wp-content/uploads/2017/04/Crazy-Gilman-1.jpg',
+                filename: 'Yelpcamp/syhrnws2zsep1cagawm2'
+            },
+            {
+                url: 'https://ww2.kqed.org/app/uploads/sites/2/2016/02/GDGilman.MAIN_.jpg',
+                filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
+            },
+            {
+                url: 'https://i1.wp.com/www.dailycal.org/assets/uploads/2014/10/gilman-inside.jpg',
+                filename: 'Yelpcamp/'
+            }
+        ],
+        description: "Famous for displaying some of the most prominent bands in the bay area. Old rosters for this venue would regularly include artists such as Green Day, Rancid, AFI, and many others. A staple for anyone interested in punk music and the DIY music scene. I have personally never been inside, but included it because on our first visit to the bay area we had the option of catching Limp Wrist here but didn’t. Still kind of bummed I missed hardcore punk icons Limp Wrist, they tour pretty infrequently.",
+        price: 5,
+        geometry: {
+            type: "Point",
+            coordinates: [
+                -122.272747,
+                37.8715926
+            ]
+        },
+        author: '604fda1ddd91d22464f03c9f'
+    },
+    {
+        location: `Fullerton, California`,
+        title: `Programme Skate and Sound`,
+        images: [
+            {
+                url: 'https://i.ytimg.com/vi/vv4J8ubegL4/maxresdefault.jpg',
+                filename: 'Yelpcamp/syhrnws2zsep1cagawm2'
+            },
+            {
+                url: 'https://pbs.twimg.com/media/DfYHk11VQAAo3uy.jpg',
+                filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
+            },
+            {
+                url: 'https://images.squarespace-cdn.com/content/v1/58025010725e2520e3591f61/1529203272062-VYTGU7H234O8UB7K65P0/ke17ZwdGBToddI8pDm48kKAwwdAfKsTlKsCcElEApLR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UegTYNQkRo-Jk4EWsyBNhwKrKLo5CceA1-Tdpfgyxoog5ck0MD3_q0rY3jFJjjoLbQ/DSC_4294.jpg?format=2500w',
+                filename: 'Yelpcamp/'
+            }
+        ],
+        description: "A skateboard shop probably better known as a punk enclave. Located in a shopping center right next to Cal State Fullerton. Whenever I’m near I’ll swing by The Hat or Bigs for some food, luckily both are open late. It’s so hard to pick a favorite show as this has been a hot spot to see many of my friend’s bands.",
+        price: 10,
+        geometry: {
+            type: "Point",
+            coordinates: [
+                -117.9145036,
+                33.8352932
+            ]
+        },
+        author: '604fda1ddd91d22464f03c9f'
+    },
+    {
+        location: `Gainesville, Florida`,
+        title: `High Dive Bar`,
+        images: [
+            {
+                url: 'https://cdn2.cloud1.cemah.net/wp-content/uploads/sites/111/2020/04/billboard-01-v2.jpg',
+                filename: 'Yelpcamp/syhrnws2zsep1cagawm2'
+            },
+            {
+                url: 'https://i2.wp.com/orangatanga.com/wp-content/uploads/2016/07/lisad02.jpg?ssl=1',
+                filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
+            },
+            {
+                url: 'https://www.ajc.com/resizer/Z8qZgyU-XNVCbMBt1-0h4rOPwuM=/1066x600/cloudfront-us-east-1.images.arcpublishing.com/ajc/XZ2OV3AEFZEDR7XUCRM3UPHR3A.jpg',
+                filename: 'Yelpcamp/'
+            }
+        ],
+        description: "You’ll know you’re in Florida if they allow you to smoke inside of venues and drink openly in the streets, all while occasionally running into gators on your trip. I remember being so exhausted I could barely enjoy Oso Oso’s set, Best Band and Best Dude. We finished off our trip with a visit to a waffle house where the chef dropped an egg and decided to take a smoke break after we placed our orders. We only waited 15 mins extra for our food but I waited a lifetime to try those Smothered and Covered Hash Browns.",
+        price: 20,
+        geometry: {
+            type: "Point",
+            coordinates: [
+                -82.32482619999999,
+                29.6516344
+            ]
+        },
+        author: '604fda1ddd91d22464f03c9f'
+    },
+    {
+        location: `New York City, New York`,
+        title: `ABC No Rio`,
+        images: [
+            {
+                url: 'https://hardcoreshowflyers.net/wp-content/uploads/2016/02/hsf000322.jpg',
+                filename: 'Yelpcamp/'
+            },
+            {
+                url: 'https://images.vice.com/noisey/content-images/article/abc-no-rio/unnamed.jpg?crop=1xw:0.8131053268765133xh;center,center&resize=1200:*',
+                filename: 'Yelpcamp/f1wcxybqf3ramyegzlwf'
+            },
+            {
+                url: 'https://images.squarespace-cdn.com/content/v1/5f6e4f12c7d9c925a9f5d36f/1601065016031-KWD6EFRUNL0GI55CVT72/ke17ZwdGBToddI8pDm48kMXRibDYMhUiookWqwUxEZ97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0luUmcNM2NMBIHLdYyXL-Jww_XBra4mrrAHD6FMA3bNKOBm5vyMDUBjVQdcIrt03OQ/ABC+No+Rio_old+facade3.jpg',
+                filename: 'Yelpcamp/syhrnws2zsep1cagawm2'
+            }
+        ],
+        description: "This venue is only here for punk credit, in hopes that an employer will value punk credit. Never been to New York",
+        price: 5,
+        geometry: {
+            type: "Point",
+            coordinates: [
+                -74.0059413,
+                40.7127837
+            ]
+        },
+        author: '604fda1ddd91d22464f03c9f'
+    }
+    
+]
